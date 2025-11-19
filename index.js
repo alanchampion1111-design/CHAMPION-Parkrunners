a25f8e5d46d907f7d769befcf5e42b0f745f58e6
Alan Champion <alan.champion1111@gmail.com>
2025-11-19 16:18:48 +0100

    Update index.js
    Check that port is 8080 is listening

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const http = require('http');
 const puppeteer = require('puppeteer');
 
 let automateBrowser = async (
@@ -13,15 +14,23 @@ let automateBrowser = async (
     userDataDir: `/mnt/c/Users/${user}/AppData/Local/Google/Chrome/User Data/${profile}`
   });
   // Automate browser interaction...
+  // return browser;    // subsequently for navigation to URL(s)
   await browser.close();
 };
 
 exports.browserAutomation = async (req, res) => {
   try {
     await automateBrowser();
+    res.writeHead(200, {'Content-Type': 'text/plain'});
     res.send('Automation complete!');
   } catch (err) {
     console.error(err);
     res.status(500).send('Error occurred');
+    res.end('Error occurred');
   }
 };
+
+const server = http.createServer(exports.browserAutomation);
+server.listen(8080, () => {
+  console.log('Server listening on port 8080');
+});
