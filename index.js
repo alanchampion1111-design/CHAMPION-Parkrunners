bf87e6cf6bd8be213dace8c62be5589398590896
Alan Champion <alan.champion1111@gmail.com>
2025-12-11 10:51:53 +0100

    Update index.js
    retry await

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ let cloudBrowser = async (
 exports.initBrowser = async () => {
   try {
     if (!initialised) {
-      cloudBrowser(7);  // Runs in the background
+      await cloudBrowser(7);  // Runs in the background
       initialised = true;
     } 
     return {
