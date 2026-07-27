c07ce38410988131a155e6842ab78cd4eb8f6bdc
Alan Champion <alan.champion1111@gmail.com>
2026-07-27 16:39:22 +0100

    Update and rename 1_Start_Node.bat to Puppet/Start_Node.bat

--- 1_Start_Node.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd C:\CHArT5k-Puppet
-node index-local.js
-pause
---
+++ Puppet/Start_Node.bat
@@ -0,0 +1,8 @@
+@echo off
+cd C:\CHArT5k-Puppet
+echo Starting CHArT5k-Puppet via PM2...
+npm start
+echo App started in background!
+echo To monitor node...
+echo npm run logs
+pause
